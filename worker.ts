780641f6af74e1d3637667e870af698866315924
Ryan <ryanyun2010@gmail.com>
2026-04-06 12:55:15 -0700

    fix

--- worker.ts
+++ worker.ts
@@ -94,7 +94,7 @@ function queryDB(
 	return ResultAsync.fromPromise(
 		db.prepare(query).bind(...binds).all(),
 		(e) => errFunc(JSON.stringify(e))
-	);
+	).map((res) => returnJSONResponse(res.results));
 }
 
 function returnJSONResponse(data: any, status: number = 200): Response {
